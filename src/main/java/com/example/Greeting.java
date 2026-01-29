c14a9568544b13dd48611c1ecd16cf16e19e07e2
Adhito <adhito909@gmail.com>
2026-01-29 22:23:04 +0700

    Create initial sample microservices

---
+++ src/main/java/com/example/Greeting.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Greeting {
+
+    private String message;
+    private String description;
+
+    public Greeting() {
+    }
+
+    public Greeting(String message, String description) {
+        this.message = message;
+        this.description = description;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
